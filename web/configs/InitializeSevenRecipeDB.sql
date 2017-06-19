6ee5a7adb1b6c8a38becfb1d35f41de5fed559ca
Ben <waripod@hotmail.com>
2017-06-19 16:27:27 -0700

    Added a basic SQL script to set up a recipes table to hold recipe information

---
+++ web/configs/InitializeSevenRecipeDB.sql
@@ -0,0 +1,15 @@
+-- Basic SQL script to initialize the Seven Recipe website database
+-- Run these commands in the shell after connecting to the database with the command
+--     "heroku pg:psql postgresql-corrugated-44518 --app seven-recipe"
+
+DROP TABLE IF EXISTS recipes;
+
+CREATE TABLE recipes(
+  name VARCHAR(40),
+  category VARCHAR(30) NOT NULL DEFAULT 'Other',
+  description VARCHAR(100) NOT NULL DEFAULT 'No description provided',
+  ingredients VARCHAR(5000) NOT NULL DEFAULT 'No ingredients given',
+  instructions VARCHAR(5000) NOT NULL DEFAULT 'No instructions given',
+  date_submitted TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW() AT TIME ZONE 'UTC',
+  PRIMARY KEY (name)
+);
