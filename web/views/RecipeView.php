639e95ec4d0e09589464c49085fce38e9c9bf194
Ben <waripod@hotmail.com>
2017-07-11 11:39:13 -0700

    Changed from exiting PHP mode in RecipeView to display content of page to echo statements

--- web/views/RecipeView.php
+++ web/views/RecipeView.php
@@ -30,16 +30,15 @@ class RecipeView extends View {
         <h1><a href="<?= $data['url'] ?>">Seven Recipe</a></h1>
         <?php
         if($data['errorFound'] === true) {  // Means query to get recipe information failed
-?>
-        <h2>An error occurred while trying to find the recipe &quot;<?= $data['recipeSearchName'] ?>&quot;. Please make sure the recipe name is valid</h2>
-        <?php}
+            echo "<h2>An error occurred while trying to find the recipe &quot;" .
+                $data['recipeSearchName'] . "&quot;. Please make sure the recipe name is valid</h2>";
+        }
         else {  // Means query to get recipe information succeeded
-?>
-        <h2><?= $data['name'] ?></h2>
-        <h3><?= $data['category'] ?></h3>
-        <h3><?= $data['description'] ?></h3>
-        <h4><?= $data['date_submitted'] ?></h4>
-        <?php
+            echo "
+            <h2>" . $data['name'] . "</h2>
+            <h3>" . $data['category'] . " ?></h3>
+            <h3>" . $data['description'] . "</h3>
+            <h4>" . $data['date_submitted'] . "</h4>";
             //Use some Element classes to generate lists for ingredients / instructions
             $ule = new UnorderedListElement();
             echo $ule->render('Ingredients', $data['ingredients']);
